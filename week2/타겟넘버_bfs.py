004d9ad14cfe7d11809b12510badbead543bcc04
sookyung kang <thoosoo@naver.com>
2023-08-10 06:09:51 +0900

    fix. 빈 파일 수정

---
+++ week2/타겟넘버_bfs.py
@@ -0,0 +1,19 @@
+#BFS
+def solution(numbers, target):
+    cnt = 0
+    leaves = [0,]
+    
+    for num in numbers:
+        temp = []
+        
+        for leaf in leaves:
+            temp.append(leaf + num)
+            temp.append(leaf - num)
+        
+        leaves = temp
+
+    for leaf in leaves:
+        if leaf == target:
+            cnt += 1
+            
+    return cnt
